8d9177fca218fcc07c53a8a13915ebd1e04dcd7e
Alex Pavelko <oo.pavelko@gmail.com>
2023-04-04 23:33:48 +0200

    add a recursive function to calculate the Fibonacci sequence

---
+++ classic-cs-problems/1-fibonacci/1-recursion.py
@@ -0,0 +1,8 @@
+def fibonacci(n: int) -> int:
+  if n < 2:
+    return n
+  return fibonacci(n - 2) + fibonacci(n - 1)
+
+if __name__ == "__main__":
+    print(fibonacci(5))
+    print(fibonacci(8))
