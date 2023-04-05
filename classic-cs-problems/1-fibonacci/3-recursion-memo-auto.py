ff500cfeb9f6c9b163d7048103d97ba2bc8d38e6
Alex Pavelko <oo.pavelko@gmail.com>
2023-04-05 20:58:25 +0200

    add a recursive function with memo to calculate the Fibonacci sequence

---
+++ classic-cs-problems/1-fibonacci/3-recursion-memo-auto.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fibonacci(n: int) -> int:
+    if n < 2:
+        return n
+    return fibonacci(n - 2) + fibonacci(n - 1)
+
+
+if __name__ == "__main__":
+    print(fibonacci(42))
